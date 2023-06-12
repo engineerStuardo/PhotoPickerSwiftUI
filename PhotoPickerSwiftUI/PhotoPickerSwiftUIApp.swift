e59847a869ae43843e5d9140481e4e36096d096c
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-06-12 10:11:53 -0600

    Initial Commit

---
+++ PhotoPickerSwiftUI/PhotoPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerSwiftUIApp.swift
+//  PhotoPickerSwiftUI
+//
+//  Created by Italo Stuardo on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
